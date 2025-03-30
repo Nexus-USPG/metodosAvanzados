deb654f9784dae9d9cefe14afbd957e24830560b
Betza <betza.otaku.karla@gmail.com>
2025-03-29 23:00:15 -0600

    Ejercicio5-Betza

---
+++ src/05-ordenarCatalogo.js
@@ -0,0 +1,16 @@
+/*Una tienda quiere mostrar su catalogo ordenado de menor a mayor precio.
+Escribe una funcion que reciba una lista de productos(nombre y precio) y los devuelva ordenados 
+por precio.[{nombre: "Mouse", precio: 20}, {nombre: "Teclado", precio: 50}, 
+{nombre: "Monitor", precio: 150}]
+*/
+const catalogo = [
+  { nombre: "Mouse", precio: 20 },  
+  { nombre: "Teclado", precio: 50 },
+  { nombre: "Monitor", precio: 150 }
+]
+
+catalogo.sort((a, b) => a.precio - b.precio)
+console.log(catalogo)
+
+catalogo.sort((a, b) => a - b)
+console.log(catalogo)
